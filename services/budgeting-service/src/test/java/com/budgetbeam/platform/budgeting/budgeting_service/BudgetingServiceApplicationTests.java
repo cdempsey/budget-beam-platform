3517153f31aad6b99ac04e17899fc8d6f1260082
Chris Dempsey <cdallas@gmail.com>
2025-10-21 17:14:08 -0500

    chore: generate the budgeting-service project

---
+++ services/budgeting-service/src/test/java/com/budgetbeam/platform/budgeting/budgeting_service/BudgetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetbeam.platform.budgeting.budgeting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
