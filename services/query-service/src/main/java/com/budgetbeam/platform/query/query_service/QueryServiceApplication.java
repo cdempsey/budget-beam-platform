a849caaa10d4b4f3fe48e7e0bd94f900aa58625c
Chris Dempsey <cdallas@gmail.com>
2025-10-21 17:16:12 -0500

    chore: generate the query-service project

---
+++ services/query-service/src/main/java/com/budgetbeam/platform/query/query_service/QueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.budgetbeam.platform.query.query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+
+}
