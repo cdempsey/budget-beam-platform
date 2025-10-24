59be21e49728e6af9bdeda31b7df1710f43902f7
Chris Dempsey <cdallas@gmail.com>
2025-10-24 16:30:20 -0500

    chore: structure project as monorepo (#2)
    * chore: lock asdf version for the project
    
    * chore: add a project .gitignore
    
    * chore: generate the enrichment-service project
    
    * chore: generate the budgeting-service project
    
    * chore: generate the query-service project
    
    * chore: generate the React UI project
    
    * chore: generate the ingestion-service project

---
+++ services/enrichment-service/src/main/java/com/budgetbeam/platform/enrichment/enrichment_service/EnrichmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.budgetbeam.platform.enrichment.enrichment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrichmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrichmentServiceApplication.class, args);
+	}
+
+}
